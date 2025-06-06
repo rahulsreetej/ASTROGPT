c1cadc140d36bd6b3bfc47198d37560458c685d5
rahul sreetej patta <rahul@rahuls-MacBook-Air.local>
2025-06-06 22:58:45 +0530

    Fix next.config.ts types

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  experimental: {
+    serverActions: {
+  bodySizeLimit: '1mb',
+  allowedOrigins: ['*'],
+}
+,
+  },
 };
 
 export default nextConfig;
